013417140254ddc9ece8af40716abd764221599a
Simon Rydell <simonwrydell@gmail.com>
2022-02-23 12:28:45 +0100

    Run npm install before configuring stage in a cross platform way

--- tests/testStage/cmake/GetNodeModules.cmake
+++ tests/testStage/cmake/GetNodeModules.cmake
@@ -2,10 +2,6 @@ include_guard()
 
 # Downloads and copies the node_modules to ./EmscriptenStage
 function(get_node_modules)
-  # Install
-  execute_process(COMMAND npm install
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-
   # node_modules
   file(COPY
     ${PROJECT_SOURCE_DIR}/node_modules
